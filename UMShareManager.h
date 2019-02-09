053b1d2b917721de05df585675c2f502f5a08bf8
郑江荣 <362675035@qq.com>
2019-02-09 23:35:37 +0800

    first

---
+++ UMShareManager.h
@@ -0,0 +1,17 @@
+//
+//  UMShareManager.h
+//  AFNetworking
+//
+//  Created by 郑江荣 on 2019/2/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UMShareManager : NSObject
++ (instancetype)sharedManager;
+-(void)initHanler;
+@end
+
+ 
